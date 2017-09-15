b8a7f6d9f0958fb98b9fa1428aeb5ddb995dd7ae
Radek <tukedarr@gmail.com>
2017-09-15 15:26:37 +0200

    AdditionalDrawing

--- my-gdx-game-core/src/com/mygdx/game/MyGdxGame.java
+++ my-gdx-game-core/src/com/mygdx/game/MyGdxGame.java
@@ -22,6 +22,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 10, 10);
+		batch.draw(img, 250, 250);
 		batch.end();
 	}
 	
